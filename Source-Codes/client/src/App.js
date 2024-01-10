79c55cd280fa4b5e5c3f7319148dd763b2f17d57
Syed Ali Mashoud <mashoudntaha@gmail.com>
2024-01-10 19:06:44 +0500

    frontend-first commit merged

--- Source-Codes/client/src/App.js
+++ Source-Codes/client/src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import GetStarted from "./Pages/GetStarted";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import Preferences from "./Pages/Preferences";
+import ProfileSettings from "./Pages/ProfileSettings";
 
 function App() {
-    return (
-        <>
-            <h1>FYP Start - Development started</h1>
-        </>
-    )
+  return (
+    <Routes>
+      <Route path="/getstarted" element={<GetStarted />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/preferences" element={<Preferences />} />
+      <Route path="/profilesettings" element={<ProfileSettings />} />
+      <Route path="/" element={<Navigate to="/getstarted" />} />
+    </Routes>
+    
+  );
 }
 
 export default App;
