2c02fd7d1cfad71e18d2a21d2ffd80d7e250748f
Hassan <hassanyahyasheikh@gmail.com>
2024-01-13 23:34:08 +0500

    created user crud backend

---
+++ Source-Codes/server/Models/user.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true, unique:true },
+  role : {type: String, required:true, },
+  preferance: {type : Strint, required:true}
+});
+
+// arg1: name of model in app, arg2: schema, arg3: collection name in db
+const User = mongoose.model('User', userSchema, "Users");
+
+module.exports = User;
