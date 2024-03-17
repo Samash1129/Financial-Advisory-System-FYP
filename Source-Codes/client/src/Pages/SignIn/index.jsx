c403321e6c3ebbdad1dcefde239d6baad4fedb16
Syed Ali Mashoud <mashoudntaha@gmail.com>
2024-03-18 00:10:51 +0500

    temp push

--- Source-Codes/client/src/Pages/SignIn/index.jsx
+++ Source-Codes/client/src/Pages/SignIn/index.jsx
@@ -5,6 +5,7 @@ import Button from '../../Components/Button';
 import NavBar from '../../Components/NavBar';
 import backgroundImage from '../../Assets/Images/background.png';
 import LoadingSpinner from '../../Components/LoadingAnimation';
+import axios from '../../axios/axios'
 
 const SignIn = () => {
 
@@ -12,7 +13,41 @@ const SignIn = () => {
   const [password, setPassword] = useState('');
   const [emailError, setEmailError] = useState('');
   const [passwordError, setPasswordError] = useState('');
+  const [credintialsError, setCredentialsError] = useState('');
   const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = axios.post('/signin', {
+        email: email,
+        password: password
+      });
+
+      // const response = fetch('https://private-61242-elev8aiapis.apiary-mock.com/signin', {
+      //   method: 'POST',
+      //   headers: {
+      //     'Content-Type': 'application/json'
+      //   },
+      //   body: JSON.stringify({
+      //     email,
+      //     password
+      //   })
+      // });
+
+
+      if ((await response).status === 200) {
+        console.log(response.data);
+        navigate('/dashregular');
+      }
+
+      setEmail('');
+      setPassword('');
+    } catch (error) {
+
+    }
+  };
 
   const validateEmail = (value) => {
     const regex = /\S+@\S+\.\S+/;
@@ -39,11 +74,6 @@ const SignIn = () => {
     }
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    navigate('/dashregular');
-  };
-
   const handleChangeToSignUp = () => {
     navigate('/signup');
   }
@@ -52,38 +82,38 @@ const SignIn = () => {
   return (
     <div className={styles.container}>
 
-    <div className={styles.leftContainer}>
-    <img src={backgroundImage} alt="Cover Image" className={styles.coverImage} />
-    </div>    
+      <div className={styles.leftContainer}>
+        <img src={backgroundImage} alt="Cover Image" className={styles.coverImage} />
+      </div>
 
-    <div className={styles.rightContainer}>
-    <LoadingSpinner loadingText="Loading...">
-    <div className={styles.signInContainer}>
-      <NavBar title="Sign In" />
-      <form className={styles.signInForm} onSubmit={handleSubmit}>
-        <label htmlFor="email">EMAIL OR NUMBER:</label>
-        <input type="text" id="email" placeholder="Enter your email address" value={email} onChange={handleEmailChange} />
-        {emailError && <div className={styles.errorMessage}>{emailError}</div>}
+      <div className={styles.rightContainer}>
+        <LoadingSpinner loadingText="Loading...">
+          <div className={styles.signInContainer}>
+            <NavBar title="Sign In" />
+            <form className={styles.signInForm} onSubmit={handleSubmit}>
+              <label htmlFor="email">EMAIL OR NUMBER:</label>
+              <input type="text" id="email" placeholder="Enter your email address" value={email} onChange={handleEmailChange} />
+              {emailError && <div className={styles.errorMessage}>{emailError}</div>}
 
-        <label htmlFor="password">PASSWORD:</label>
-        <input type="password" id="password" placeholder="Enter your 8-digit password" value={password} onChange={handlePasswordChange} />
-        {passwordError && <div className={styles.errorMessage}>{passwordError}</div>}
+              <label htmlFor="password">PASSWORD:</label>
+              <input type="password" id="password" placeholder="Enter your 8-digit password" value={password} onChange={handlePasswordChange} />
+              {passwordError && <div className={styles.errorMessage}>{passwordError}</div>}
 
-        <a href="/forgot-password" className={styles.forgotPasswordLink}>Forgot your password?</a>
-        
-        <Button text="Log In"/>
-        <div className={styles.secondaryAction}>
-          Need an account? 
-          <p onClick={handleChangeToSignUp} className={styles.signUpLink}>Sign up</p>
-        </div>
-      </form>
+              <a href="/forgot-password" className={styles.forgotPasswordLink}>Forgot your password?</a>
+
+              <Button text="Log In" />
+              <div className={styles.secondaryAction}>
+                Need an account?
+                <p onClick={handleChangeToSignUp} className={styles.signUpLink}>Sign up</p>
+              </div>
+            </form>
+          </div>
+        </LoadingSpinner>
+      </div>
+
     </div>
-    </LoadingSpinner>
-    </div>
 
-    </div>
-
-);
+  );
 };
 
 export default SignIn;
